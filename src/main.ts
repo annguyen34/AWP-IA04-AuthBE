39300688e474f08ae927cbd64e3d1c4501bda438
annguyen34 <21120508@student.hcmus.edu.vn>
2024-11-06 10:22:16 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: 'https://cheerful-kataifi-bd0c60.netlify.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
   });
   await app.listen(3000);
 }
